0f1f63ee448f84772e53ef476ef66b2a2927d797
xuanbai01 <xuanbai@ad.unc.edu>
2025-09-16 23:20:52 -0400

    quick fix for deloyment

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/Editor/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/Editor/page.tsx
@@ -1,7 +1,7 @@
-// app/(Kambaz)/Courses/[cid]/Assignments/[aid]/Editor/page.tsx
 import Editor from "./editor";
 
-export default function Page(props: any) {
-  // Pass through params if/when you need them later
-  return <Editor {...props} />;
+export default function Page({
+  params,
+}: { params: { cid: string; aid: string } }) {
+  return <Editor />;
 }
--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -55,7 +55,7 @@ export default function Lab1() {
                 <ul id="wd-my-books">
                     <li>Dune</li>
                     <li>Lord of the Rings</li>
-                    <li>Ender's Game</li>
+                    <li>Ender&apos;s Game</li> 
                     <li>Red Mars</li>
                     <li>The Forever War</li>
                 </ul>
