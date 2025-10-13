745ebb01cd19e6199d2bb2b7ab5cecb5427adac0
xuanbai01 <xuanbai@ad.unc.edu>
2025-10-13 18:22:25 -0400

    Fix ESLint errors

--- app/Labs/Lab3/todos/TodoList.tsx
+++ app/Labs/Lab3/todos/TodoList.tsx
@@ -1,14 +1,16 @@
 import { ListGroup } from "react-bootstrap";
 import TodoItem from "./TodoItem";
 import todos from "./todos.json";
-export default function TodoList() {
- return(
-   <>
-     <h3>Todo List</h3>
-     <ListGroup>
-       { todos.map(todo => {
-           return(<TodoItem todo={todo}/>);   })}
-     </ListGroup><hr/>
-   </>
-);}
 
+export default function TodoList() {
+  return(
+    <>
+      <h3>Todo List</h3>
+      <ListGroup>
+        {todos.map((todo, index) => {
+          return(<TodoItem key={index} todo={todo}/>);
+        })}
+      </ListGroup><hr/>
+    </>
+  );
+}
