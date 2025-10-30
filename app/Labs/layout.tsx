4484d1bc120cbcc328ec7f7f5a87f0cbae1a2f1e
xuanbai01 <xuanbai@ad.unc.edu>
2025-10-29 21:19:15 -0400

    add labs layout provider

--- app/Labs/layout.tsx
+++ app/Labs/layout.tsx
@@ -1,21 +1,26 @@
+"use client";
+
 import { ReactNode } from "react";
 import TOC from "./TOC";
+import { Provider } from "react-redux";
+import store from "@/app/(Kambaz)/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 export default function LabsLayout({
- children,
+  children,
 }: Readonly<{ children: ReactNode }>) {
- return (
-   <table>
-     <tbody>
-       <tr>
-         <td valign="top" width="100px">
-           <TOC />
-         </td>
-         <td valign="top">{children}</td>
-       </tr>
-     </tbody>
-   </table>
-);}
-
+  return (
+    <Provider store={store}>
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" width="100px">
+              <TOC />
+            </td>
+            <td valign="top">{children}</td>
+          </tr>
+        </tbody>
+      </table>
+    </Provider>
+  );
+}
